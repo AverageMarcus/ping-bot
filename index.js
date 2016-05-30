5bc4240abedba3ef6065900fdb64765941f342ca
Marcus Noble <github@marcusnoble.co.uk>
2016-05-30 11:30:42 +0100

    PingPong slash command

--- index.js
+++ index.js
@@ -18,47 +18,6 @@ server.route({
   }
 });
 
-server.route({
-  method: 'POST',
-  path: '/tableflip',
-  handler: (request, reply) => {
-    console.log(request.payload);
-    console.log(typeof request.payload);
-
-    let slackURL = url.parse(request.payload.response_url);
-    let echoChannel = request.payload.channel_id;
-    let usersText = request.payload.text.trim();
-
-    let output = JSON.stringify({
-      token: request.payload.token,
-      channel: request.payload.channel_id,
-      text: `${usersText} ()`,
-      as_user: true
-    });
-
-    let options = {
-      host: 'slack.com',
-      path: '/api/chat.postMessage',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': output.length
-      }
-    };
-    let post = https.request(options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function(chunk) {
-        console.log('response', chunk);
-      });
-    });
-
-    post.write(output);
-    post.end();
-
-    reply();
-  }
-});
-
 server.start((err) => {
   if (err) {
     throw err;
