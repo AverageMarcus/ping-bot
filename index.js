877649e6ac8db71d754ea286cdfc2bb299617d8e
Marcus Noble <github@marcusnoble.co.uk>
2016-05-30 11:35:03 +0100

    Renamed

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 'use strict';
-const url = require('url');
-const https = require('https');
-
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000 });
